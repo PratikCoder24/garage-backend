4313f94a7e49d6d492668d14f2162541640d8b43
pratik <pratikshenoy24@gmail.com>
2026-06-20 00:58:44 +0530

    feat(exception): add global exception handler for validation and error responses

---
+++ src/main/java/Garage_System/exception/GlobalExceptionHandler.java
@@ -0,0 +1,88 @@
+package Garage_System.exception;
+
+import Garage_System.DTO.ResponseDTO.ErrorResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponseDTO> handleResourceNotFoundException(ResourceNotFoundException ex){
+        return new ResponseEntity<>(
+                new ErrorResponseDTO(
+                        LocalDateTime.now().toString(),
+                        HttpStatus.NOT_FOUND.value(),
+                        "Not Found",
+                        ex.getMessage()
+                ),
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponseDTO> handleDuplicateResourceException(DuplicateResourceException ex){
+        return new ResponseEntity<>(
+                new ErrorResponseDTO(
+                        LocalDateTime.now().toString(),
+                        HttpStatus.CONFLICT.value(),
+                        "Conflict",
+                        ex.getMessage()
+                ),
+                HttpStatus.CONFLICT
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationException(MethodArgumentNotValidException ex){
+        Map<String,String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.put(
+                                error.getField(),
+                                error.getDefaultMessage()
+                        )
+                );
+
+        return new ResponseEntity<>(
+                errors,
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponseDTO> handleInvalidJSON(HttpMessageNotReadableException ex){
+        return new ResponseEntity<>(
+                new ErrorResponseDTO(
+                        LocalDateTime.now().toString(),
+                        HttpStatus.BAD_REQUEST.value(),
+                        "Bad Request",
+                        "Invalid JSON format or missing request body"
+                ),
+          HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleGenericException(){
+        return new ResponseEntity<>(
+                new ErrorResponseDTO(
+                        LocalDateTime.now().toString(),
+                        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                        "Internal Server Error",
+                        "Something went wrong. Please try again!"
+                ),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+}
---
+++ src/main/java/Garage_System/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package Garage_System.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+
+}
